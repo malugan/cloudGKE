8b5139da12a462cc7747ea950f44f18535bbac66
malugan <wangsa.fr@gmail.com>
2022-04-16 00:52:44 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Frans Junior Wangsa \n')
+    res.send("Frans Junior Wangsa \n51019009 \nSistem Informasi \nhttps://m.facebook.com/login/?locale=id_ID")
 })
 
 app.listen(PORT, HOST)
